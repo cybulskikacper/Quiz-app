c29fba70c9628a4fc38eab95cd589dae27df93cb
Kacper <kacpercybulski@icloud.com>
2023-04-15 21:50:50 +0200

    site refreshing after clicking button is blocked,checking which answers are clicked,checking if all answers are selected

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const form = document.querySelector('.quiz-box')
-const answers = document.querySelectorAll('.answer')
+const answers = Array.from(document.querySelectorAll('.answer'))
 
 const modal = document.querySelector('.modal')
 const modalInfo = modal.querySelector('p')
 const modalBtn = modal.querySelector('.close-modal')
 
+const allQuestions = document.querySelectorAll('.question')
+
+const handleQuiz = e => {
+	e.preventDefault()
+
+	const checkedAnswers = answers.filter(answer => answer.checked)
+	const isTrue = checkedAnswers.every(answer => answer.value === 'true')
+	const allChecked = checkedAnswers.length === allQuestions.length
+	console.log(allChecked)
+
+	if (!allChecked) {
+        modal.classList.add('modal-active')
+    
+    }
+}
+
+form.addEventListener('submit', handleQuiz)
