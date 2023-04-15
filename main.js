133e12fc63f96e921d4e065e73e6b50d17256dd0
Kacper <kacpercybulski@icloud.com>
2023-04-15 12:16:12 +0200

    downloaded all stuff needed using query selector

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.quiz-box')
+const answers = document.querySelectorAll('.answer')
+
+const modal = document.querySelector('.modal')
+const modalInfo = modal.querySelector('p')
+const modalBtn = modal.querySelector('.close-modal')
+
